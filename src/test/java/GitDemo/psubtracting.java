6ac4d1d86d36a798139940059532fd0ec1afad56
Praveenaaraja23 <praveenaaraja23@gmail.com>
2024-11-29 17:10:06 -0500

    subtraction changed

--- src/test/java/GitDemo/psubtracting.java
+++ src/test/java/GitDemo/psubtracting.java
@@ -2,7 +2,7 @@ package GitDemo;
 
 public class psubtracting {
 	public static void main(String[] args) {
-		System.out.println("subtraction");
+		System.out.println("subtraction changed by praveena");
 	}
 
 }
